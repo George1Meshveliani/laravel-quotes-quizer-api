e1089d8ff65e0679a92edf1fc44bb5c5bf888ee2
George1Meshveliani <giorgi.meshveliani@gmail.com>
2022-06-18 13:51:01 +0400

    #14 - update quotes migration

--- database/migrations/2022_06_18_091513_crete_quotes_table.php
+++ database/migrations/2022_06_18_091513_crete_quotes_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('author');
+            $table->rememberToken();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('quotes');
     }
 };
